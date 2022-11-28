c9b62acc0a498225f25a59dab0ce785b6d614d4c
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-28 13:49:47 +0000

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[7:]
 middle_word = word[1:-1]
-print("First 3 letters: %s " % word_first_3)
-print("Last 2 letters: %s " % word_last_2)
-print("Middle word: %s" % middle_word)
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
