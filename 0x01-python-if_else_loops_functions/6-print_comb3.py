2c5ba8ef96d3311a1eab6d25039b3d30f65d7d5a
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-29 13:29:14 +0000

    fix, pycodestyle validation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 num1 = 0
 num2 = 0
-for num1 in range(0,9):
+for num1 in range(0, 9):
     for num2 in range((num1 + 1), 10):
         if num1 != 8:
-            print("{}{}".format(num1,num2), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
         else:
-            print("{}{}".format(num1,num2))
+            print("{}{}".format(num1, num2))
