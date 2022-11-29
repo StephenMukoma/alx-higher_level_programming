75c7f0afa59c62252140cc42bb3ff55f43ac53e2
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-29 13:26:21 +0000

    fix error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 num1 = 0
 num2 = 0
-for num1 in range(0,10):
-	for num2 in range(0,10):
-		if num1 > num2 or num1 == num2:
-			pass			
-		else:
-			print("{}{}".format(num1,num2), end=', ')
-			print("", end="")
+for num1 in range(0,9):
+    for num2 in range((num1 + 1), 10):
+        if num1 != 8:
+            print("{}{}".format(num1,num2), end=", ")
+        else:
+            print("{}{}".format(num1,num2))
