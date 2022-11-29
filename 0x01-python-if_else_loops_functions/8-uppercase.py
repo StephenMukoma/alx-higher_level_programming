67dfad72f9fe0051b4b9b6407cb523e286e392ff
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-29 14:20:40 +0000

    feat,prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    output = ''
+    for char in str:
+        if ord(char) >=97 and ord(char) <= 122:
+            output += chr(ord(char) - 32)
+        else:
+            output += char
+    print("{}".format(output))
+
