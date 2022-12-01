af80cda55db6c25b4142cc64e6ddc85057a15599
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-01 11:58:14 +0000

    fix code requirement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
+if __name__==""__main__"":
+    from add_0 import add
+    a = 1
+    b = 2
 
-total = add(a, b)
+    total = add(a, b)
 
-print("{:d} + {:d} = {:d}".format(a,b,total))
+    print("{:d} + {:d} = {:d}".format(a,b,total))
