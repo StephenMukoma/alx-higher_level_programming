ff26616044564e2ccd91b4f4e81759a11fc3a74b
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-01 13:57:56 +0000

    print all names compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    hidden_discovery = dir(hidden_4)
+    for n in range(len(hidden_discovery)):
+        if hidden_discovery[n][0] != '_' and hidden_discovery[n][1] != '-':
+            print(hidden_discover[n])
