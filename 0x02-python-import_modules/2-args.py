a65692efde1106fafc6144f5226764def3856ead
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-01 13:00:37 +0000

    fix, code requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-n = len(sys.argv)
-n = n - 1
+    n = len(sys.argv)
+    n = n - 1
 
-if n == 0:
-    print("{} arguments.".format(n))
-elif n == 1:
-    print("{} argument:".format(n))
-    print("{}: {}".format(n,sys.argv[1]))
-else:
-    print("{} arguments:".format(n))
-    for i in range(n + 1):
-        print("{}: {}".format(i,sys.argv[i]))
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("{}: {}".format(n,sys.argv[1]))
+    else:
+        print("{} arguments:".format(n))
+        for i in range(1, n + 1):
+            print("{}: {}".format(i,sys.argv[i]))
