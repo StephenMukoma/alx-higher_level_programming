b314edb2ab2b61eadf2481612ff3e53eee711988
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 14:11:46 +0000

    feat, find biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    
+    elif length > 0:
+        max = my_list[0]
+        for i in my_list:
+            if i > max:
+                max = i
+    return max        
