1449c65b6f5edb7baa95ce41dd5354c473c12915
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 08:45:41 +0000

    fix, code requirement fail

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for i in range(5):
-            print(my_list[i])
+def print_list_integer(my_list=[]):
+    for i in range(5):
+        print("{:d}".format(my_list[i]))
