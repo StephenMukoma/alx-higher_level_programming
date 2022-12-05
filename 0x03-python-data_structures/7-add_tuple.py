4fcf692bddef44ce0f6047fed497b65e03999ac9
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 13:22:50 +0000

    fix, requirement code fail

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup_1 = list(tuple_a)
-    tup_2 = list(tuple_b)
-    if len(tup_1) < 2:
-        if len(tup_1) == 1:
-            tup_1.append(0)
+    tuple1 = list(tuple_a)
+    tuple2 = list(tuple_b)
+
+    if len(tuple1) < 2:
+        if tuple1 == 1:
+            tuple1.append(0)
         else:
-             tup_1.append(0)
-             tup_1.append(0)
-    if len(tup_2) < 2:
-        if len(tup_2) == 1:
-            tup_2.append(0)
+            tuple1.append(0)
+            tuple1.append(0)
+
+    if len(tuple2) < 2:
+        if tuple2 == 1:
+            tuple2.append(0)
         else:
-            tup_2.append(0)
-            tup_2.append(0)
-    return (tup_1[0] + tup_2[0]), (tup_1[1] + tup_2[1])
+            tuple2.append(0)
+            tuple2.append(0)
+
+    return (tuple1[0] + tuple2[0]), (tuple1[1] + tuple2[1])
