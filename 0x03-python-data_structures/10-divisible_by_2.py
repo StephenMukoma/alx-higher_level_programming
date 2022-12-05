7fff600b5ebfe5e5cc9dced1d1f265524f531e93
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 14:34:06 +0000

    feat, find multiples of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    length = len(my_list)
+    list_result = list()
+    for i in range (0, length - 1):
+        if my_list[i] % 2 == 0:
+            list_result.append(True)
+        else:
+            list_result.append(False)
+    return list_result
