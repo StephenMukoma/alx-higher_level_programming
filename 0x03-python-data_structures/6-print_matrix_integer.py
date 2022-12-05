ffb0e7dae32875b8438635d6bcc1712ac5d46cbf
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 12:00:19 +0000

    feat, print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        x = '\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in matrix])
-        print(x)
+    for row in range(len(matrix)):
+        for item in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][item]), end="")
+            if item != len(matrix[row]) - 1:
+                print(" ", end="")
+        print("") 
