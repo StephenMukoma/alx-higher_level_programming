995ee2443acefe887d5faaa5db0bef1a0907c7aa
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 14:13:05 +0000

    fix , pycodestyle error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@ def max_integer(my_list=[]):
     length = len(my_list)
     if length == 0:
         return None
-    
+
     elif length > 0:
         max = my_list[0]
         for i in my_list:
             if i > max:
                 max = i
-    return max        
+    return max
