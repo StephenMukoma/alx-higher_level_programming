dc1a0e2963ea3622449cbc1a7775154d390763a0
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-05 14:59:28 +0000

    fix , requirement fail

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,8 +4,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        for i in range (0, len(my_list) - 1):
-            if i == 3:
-                pass
-            new_list.append(my_list[i])
-    return new_list
+        del my_list[idx]
+    return my_list
