6c4abe7f42cf56dc2256353b4b9ccbc48e1ae2f9
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-07 14:20:55 +0000

    fix, code fail requirements

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
-            return None
-    else:
-        max_value = max(a_dictionary)
-    return max_value
+    if a_dictionary:
+        key, value = max(a_dictionary.items(), key=lambda x: x[1])
+        return key
