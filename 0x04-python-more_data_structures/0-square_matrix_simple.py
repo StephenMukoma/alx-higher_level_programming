5a08d44161ff7ad3e211843fcebc2842030fb82e
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-07 10:47:57 +0000

    fix, pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     square = []
     for i in matrix:
-        s = list(map(lambda x : x ** 2, i))
+        s = list(map(lambda x: x ** 2, i))
         square.append(s)
     return square
