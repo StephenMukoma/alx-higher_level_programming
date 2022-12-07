8997e57449ce9afb6121f8e92d7d2d99981c6b0c
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-07 18:04:19 +0000

    converts roman numeral to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str and roman_string:
+        number = []
+        for i in roman_string:
+            if i == 'I':
+                number.append(1)
+            elif i == 'V':
+                number.append(5)
+            elif i == 'X':
+                number.append(10)
+            elif i == 'C':
+                number.append(50)
+            elif i == 'D':
+                number.append(100)
+            elif i == 'M':
+                number.append(1000)
+        for i in range(len(number)):
+            if i != len(number) - 1:
+                if number[i] < number[i + 1]:
+                    number[i] = -number[i]
+        return sum(number)
+    else:
+         return 0
