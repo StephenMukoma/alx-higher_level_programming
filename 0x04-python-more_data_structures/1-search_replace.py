f2d2f1e7ca796283aa23bf59f0a477e4692207fd
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-07 11:02:45 +0000

    fix, requirement fail

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
     length = len(my_list)
     new_list = []
     for i in my_list:
-        if i == 2:
-            i = 89
+        if i == search:
+            i = replace
             new_list.append(i)
         else:
             new_list.append(i)
