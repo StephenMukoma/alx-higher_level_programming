f420df5c53402fd3b6a7faffcff0840fc9a5cdc7
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-19 11:51:29 +0000

    fix, code requirement fail

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,15 +3,15 @@ def safe_print_list(my_list=[], x=0):
     if my_list:
         length = 0
 
-    try:
-        for i in range(0, x):
-            print("{}".format(my_list[i]), end="")
-            length += 1
+        try:
+            for i in range(0, x):
+                print("{}".format(my_list[i]), end="")
+                length += 1
             print()
             return length
-    except ValueError:
-       print()
-       return length
+        except IndexError:
+            print()
+            return length
     else:
         print()
         return 0
