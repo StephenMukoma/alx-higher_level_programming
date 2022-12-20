f3988ce47aa4a68969c0f53a940ee843e6c7b23a
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-20 11:57:44 +0000

    feat, class that defines square by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """Declares a square"""
+
+
+    def __init__(self, size):
+        """Initialize a square"""
+
+
+        self.__size = size
