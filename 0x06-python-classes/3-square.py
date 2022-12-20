60ff6fcb892b85e728b88d7dc5c3871937e62f70
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-20 12:16:49 +0000

    feat, public instance method, returns current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Declare a square"""
+
+
+class Square:
+    """Represent a square"""
+
+
+    def __init__(self, size=0):
+        """Initialize a square
+            Args:
+                size: size of Square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+    def area(self):
+        return self.__size ** 2
