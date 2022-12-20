c4de564fa0c63106dd9ea33496407d23326d33e4
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-20 12:57:06 +0000

    feat, coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Declares a square"""
+
+
+class Square:
+    """Represents a square"""
+
+
+    def __init__(self, size=0):
+        """Initialize a square
+
+
+            Args:
+                size: size of square
+
+
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
+
+            for i in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
