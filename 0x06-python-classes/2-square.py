b9b2f75077eb28a65f1112d2e14c2f6d25ba90d8
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-20 12:11:20 +0000

    fix, requirement fail

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""Declare a square"""
+
 
 class Square:
+    """Represent a square"""
+
+
     def __init__(self, size=0):
+        """Initialize a square
+
+
+            Args:
+                size: size of Square
+
+
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
+        else:
+            self.__size = size
