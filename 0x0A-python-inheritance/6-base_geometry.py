ca065b0aea11a9467c990be60969562350545b20
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2023-01-09 16:45:02 +0000

    class BaseGeometry (based on 5-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Representing BaseGeometry"""
+    def area(self):
+        """Raises exception"""
+        raise Exception("area() is not implemented")
