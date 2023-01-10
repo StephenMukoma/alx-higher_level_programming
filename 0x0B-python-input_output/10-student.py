105711e9dd40835b20f6357e39b73d076dc7877e
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2023-01-10 18:55:01 +0000

    fix pycodestyle

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,7 +19,7 @@ class Student:
     def to_json(self, attrs=None):
         """Return dictionary representation of Student"""
         if isinstance(attrs, list) and\
-            all(isinstance(item, str) for item in attrs):
+        all(isinstance(item, str) for item in attrs):
             result = {}
             for i in attrs:
                 try:
